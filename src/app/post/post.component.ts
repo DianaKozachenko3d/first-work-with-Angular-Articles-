6ee4d330023c9f10b7cb659e8d5ab710403ae68f
dnnsmn <dnnsmnkozachenko@gmail.com>
2019-11-27 23:13:38 +0200

    *ngFor for my blocks

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -2,7 +2,28 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'app-post',
+    template:`<ul> 
+                    <li *ngFor="let item of items">{{items}}</li>
+              <ul>`,
     templateUrl: './post.component.html',
     styleUrls: ['./post.component.css']
 })
-export class PostComponent {}
+export class PostComponent {
+
+    titles = [
+        {
+            property:'Post title intell',
+            definition: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum in itaque rerum tenetur doloremque placeat excepturi esse repellendus totam! Adipisci!'
+        },
+        {
+            property:'Post title magic',
+            definition: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum in itaque rerum tenetur doloremque placeat excepturi esse repellendus totam! Adipisci!'
+        },
+        {
+            property:'Post title silicon',
+            definition:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum in itaque rerum tenetur doloremque placeat excepturi esse repellendus totam! Adipisci!'
+
+        }
+    ]
+
+}
